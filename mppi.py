e144be754a18b7a97695c9d1681b190c5521f361
Jaisel Singh <singhjaisel01@gmail.com>
2025-07-20 19:18:59 -0700

    rendering, need to fix camera

--- mppi.py
+++ mppi.py
@@ -1,7 +1,8 @@
 import ur5_env as env
 
-ur5 = env.ur5()
+ur5 = env.ur5(render_mode="human")
 ur5.reset()
 
-obs, reward, done, term, info = ur5.step(ur5.action_space.sample())
-print(obs)
+# iterable that has a next method that can run x times
+for step in range(100):
+    obs, reward, done, term, info = ur5.step(ur5.action_space.sample())
