4c0dae96ff08321e4939bafab7b96717fa61dab1
Jaisel Singh <singhjaisel01@gmail.com>
2025-11-19 15:55:24 -0500

    adding JAX based cost

--- scripts/vis_mppi.py
+++ scripts/vis_mppi.py
@@ -1,4 +1,7 @@
 import mujoco 
+import mujoco.mjx as mjx 
+import jax 
+from jax.numpy import jnp
 from jaxtyping import Float
 import numpy as np
 from mppi_gps.controllers.mppi import MPPI 
@@ -15,6 +18,51 @@ def cost(states: Float[np.ndarray, "K T S"]) -> Float[np.ndarray, "K"]:
     dist = np.sqrt((-1.5-joint)**2)
     cost = np.sum(dist, axis=1)
     return cost
+
+def vec_pick_place_cost(states: Float[np.ndarray, "K T S"]) -> Float[np.ndarray, "K"]:
+    K, T, S = states.shape 
+    target_lift_height = 0.50 
+
+    states_flat = states.reshape(K*T, S)
+    nq = planner_env.model.nq 
+
+    qpos_all = states_flat[:, 1+nq+1]
+
+    # for the forward kinematics
+    tcp_locs = np.zeros((K*T, 3))
+    can_locs = np.zeros((K*T, 3))
+
+    # you cannot do the numpy for the forward part with the simulator since you need to create multiple simulator instances 
+    data_batch = compute_fk(qpos_all)
+
+    tcp_site_id = mujoco.mj_name2id(planner_env.model, mujoco.mjtObj.mjOBJ_SITE, "link_tcp")
+    can_body_id = mujoco.mj_name2id(planner_env.model, mujoco.mjtObj.mjOBJ_BODY, "can")
+    
+    tcp_locs = data_batch.site_xpos[:, tcp_site_id]  # (K*T, 3)
+    can_locs = data_batch.xpos[:, can_body_id]  # (K*T, 3)
+
+    dist_tcp_can = np.linalg.norm(can_locs - tcp_locs, axis=1)
+    can_heights = can_locs[:, 2]
+    lift_penalty = 100 * (target_lift_height - can_heights)**2
+
+    costs_flat = dist_tcp_can + lift_penalty  # Shape: (K*T,)
+    costs_per_dt = costs_flat.reshape(K, T)
+    trajectory_costs = np.sum(costs_per_dt, axis=1)  # Shape: (K,)
+
+    return trajectory_costs
+
+mjx_model = mjx.put_model(planner_env.model)
+def compute_fk(qpos_all):
+    # qpos_all shape (K*T, S)
+    # takes the function lambda and then applies it into every row of qpos_all 
+
+    # the qpos_all in the second brackets is calling the vectorized function 
+    data_batch = jax.vmap(lambda qpos: mjx.make_data(mjx_model))(qpos_all)
+    
+    # JAX requires immutability 
+    data_batch = jax.vmap(lambda d,q: mjx.forward(mjx_model, d.replace(qpos=q)))(data_batch, qpos_all)
+    return data_batch
+
 
 def pick_place_cost(states: Float[np.ndarray, "K T S"]) -> Float[np.ndarray, "K"]:
     traj_costs = np.zeros([states.shape[0], states.shape[1]])
@@ -50,7 +98,7 @@ def pick_place_cost(states: Float[np.ndarray, "K T S"]) -> Float[np.ndarray, "K"
     return costs 
 
 
-controller = MPPI(planner_env, pick_place_cost)
+controller = MPPI(planner_env, vec_pick_place_cost)
 # farama env needs to reset before
 env.reset()
 for step in range(1000):
