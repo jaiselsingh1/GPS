20d6c8afefe6b5b586d122a54d82ab3dc23c97e7
Jaisel Singh <singhjaisel01@gmail.com>
2025-11-20 17:56:35 -0500

    adding to jax_mppi

--- src/mppi_gps/controllers/jax_mppi.py
+++ src/mppi_gps/controllers/jax_mppi.py
@@ -7,4 +7,38 @@ from jaxtyping import Float
 import numpy as np
 from collections import abc 
 
+class MPPI_JAX:
+    def __init__(
+        self, 
+        env: MujocoEnv,
+        cost: abc.Callable,  # Cost function that takes JAX array as input (aka states as jnp)
+        num_samples: int = 64,
+        horizon: int = 16,
+        noise_sigma: float = 0.01,
+        lambda_: float = 0.00001,
+        frame_skip: int = 5,
+    ):
+        self.num_samples = num_samples 
+        self.horizon = horizon 
+        self.noise_sigma = noise_sigma 
+        self.lambda_ = lambda_
+        self.frame_skip = frame_skip
 
+        env.reset()
+        self.mjx_model = mjx.put_model(env.model)
+        self.act_dim = env.action_space.shape[0]
+
+        init_control = jnp.array(self.act_dim)
+        # tile repeats along the current dimensions 
+        self.U = jnp.tile(init_control, (self.horizon, 1)) # self.horizon, 1 tells you how to repeat along each direction 
+
+        # random key for JAX (needed for when you want to do JIT)
+        self.key = jax.random.PRNGKey(0)
+
+        self._compile_rollout()
+
+    
+    
+
+
+        
