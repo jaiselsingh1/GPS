19c2da5d637f19ae21f8ea3a7a00a5a60a178c55
Jaisel Singh <singhjaisel01@gmail.com>
2025-12-03 14:34:53 -0500

    new scenes

--- scripts/vis_env.py
+++ scripts/vis_env.py
@@ -2,19 +2,19 @@ import time
 import pathlib
 import numpy as np
 from mppi_gps.envs.xarm7_env import Xarm7
-from mppi_gps.envs.robosuite_xarm7 import make_pick_place_model
+# from mppi_gps.envs.robosuite_xarm7 import make_pick_place_model
 
 cur_path = pathlib.Path(__file__)
 mppi_gps_dir = cur_path.parent.parent
 
 # point to the actual models/assets location
-xml_rel_path = mppi_gps_dir / "src/mppi_gps/models/assets/scene_robosuite.xml"
+xml_rel_path = mppi_gps_dir / "src/mppi_gps/models/assets/plate_dishwasher_task.xml"
 xml_rel_path.parent.mkdir(parents=True, exist_ok=True)
 
-make_pick_place_model(save_xml_path=xml_rel_path)
+# make_pick_place_model(save_xml_path=xml_rel_path)
 
 # point Xarm7 at that XML - use the path relative to src/mppi_gps
-env = Xarm7(model_path="models/assets/scene_robosuite.xml", render_mode="human")
+env = Xarm7(model_path=xml_rel_path, render_mode="human")
 
 steps = 1000
 obs, _ = env.reset()
